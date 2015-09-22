0c4c8db206b4a28c29089fe37d13075db16b98eb
llx <495134387@qq.com>
2015-09-22 23:06:20 +0800

    [llx] test for jenkins

--- src/main/java/HelloWorldImp.java
+++ src/main/java/HelloWorldImp.java
@@ -2,11 +2,11 @@
  * Created by lxliang on 9/21/15.
  */
 public class HelloWorldImp implements HelloWorldInterface {
-    public String sayHello(String username){
+    public String sayHello(String username) {
         return "Hello  " + username;
     }
 
-    public void setUser(String username){
+    public void setUser(String username) {
 
     }
 }
--- src/main/java/HelloWorldInterface.java
+++ src/main/java/HelloWorldInterface.java
@@ -2,22 +2,19 @@ import javax.jws.WebService;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-/**
- * Created by lxliang on 9/21/15.
- */
 @WebService
 public interface HelloWorldInterface {
 
     @GET
-    @Path("/get/{username}")
+    @Path("/personDetail/{username}")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces({ MediaType.APPLICATION_JSON })
+    @Produces({MediaType.APPLICATION_JSON})
     public String sayHello(@PathParam(value = "username") String username);
 
     @POST
-    @Path("post")
+    @Path("updatePersonDetail")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces({ MediaType.APPLICATION_JSON })
+    @Produces({MediaType.APPLICATION_JSON})
     public void setUser(String username);
 
 
